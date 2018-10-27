2abb244dd45c339285b3a289c46ada0df1b02ec4
Eric Pulvino <ericpulvino@gmail.com>
2018-10-27 17:08:28 -0400

    Updates for 3.7.0 deploy.

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,7 @@
 sudo apt-get install -qy  python-dev libssl-dev  sshpass build-essential libffi-dev
 sudo pip install setuptools --upgrade
-sudo pip install ansible==2.4.1.0
+# We have to do this here because Ansible cannot upgrade ansible without bombs going off.
+sudo pip install ansible==2.5.4
 rm -rf ansible-push-keys
 git clone https://github.com/cumulusnetworks/ansible-push-keys
 cd ansible-push-keys; cat /etc/dhcp/dhcpd.hosts | grep 'host .* {' | cut -d " " -f 2 >> hosts
