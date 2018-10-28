9863ffcb4bd011b6e811a25a5ecbe3108149132c
Eric Pulvino <ericpulvino@gmail.com>
2018-10-27 22:12:50 -0400

    Updates for Ansible version to avoid https://github.com/ansible/ansible/issues/41717 .

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,7 @@
 sudo apt-get install -qy  python-dev libssl-dev  sshpass build-essential libffi-dev
 sudo pip install setuptools --upgrade
 # We have to do this here because Ansible cannot upgrade ansible without bombs going off.
-sudo pip install ansible==2.5.4
+sudo pip install ansible==2.5.10
 rm -rf ansible-push-keys
 git clone https://github.com/cumulusnetworks/ansible-push-keys
 cd ansible-push-keys; cat /etc/dhcp/dhcpd.hosts | grep 'host .* {' | cut -d " " -f 2 >> hosts
